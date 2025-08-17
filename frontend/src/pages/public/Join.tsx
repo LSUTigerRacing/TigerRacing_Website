fec9da6d6fa6ae9d5a48ad51088ab064c3d83708
Ricky Liang <liangricky7@gmail.com>
2025-08-16 21:06:58 -0500

    join page initialize

--- frontend/src/pages/public/Join.tsx
+++ frontend/src/pages/public/Join.tsx
@@ -1,7 +1,10 @@
 const Join = () => {
   return (
     <div>
-
+      <span className="text-center text-[#510087] text-[6vw]">
+        <h1 className="">Want to Join?</h1>
+        <p>All Majors Welcome!</p>
+      </span>
     </div>
   );
 };
