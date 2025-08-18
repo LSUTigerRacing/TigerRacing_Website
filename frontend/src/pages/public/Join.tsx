6330cbab03e35e6ae09d2948bb071cb666a95310
Ricky Liang <liangricky7@gmail.com>
2025-08-18 06:14:18 -0500

    join page font resizing
     paused because margins arent working

--- frontend/src/pages/public/Join.tsx
+++ frontend/src/pages/public/Join.tsx
@@ -1,10 +1,25 @@
 const Join = () => {
   return (
     <div>
-      <span className="text-center text-[#510087] text-[6vw]">
-        <h1 className="">Want to Join?</h1>
-        <p>All Majors Welcome!</p>
+      <span className="w-screen text-center text-[#510087] inline-block mt-60">
+        <span className="text-[10rem]">
+          <h1 >Want to Join?</h1>
+        </span>
+        <span className="text-[3.5rem]">
+          <p>All Majors Welcome!</p>
+        </span>
       </span>
+      <div className="w-[93vw] flex justify-between">
+        <div>
+          CHASSIS
+        </div>
+        <div>
+          POWERTRAIN
+        </div>
+        <div>
+          BUSINESS
+        </div>
+      </div>
     </div>
   );
 };
