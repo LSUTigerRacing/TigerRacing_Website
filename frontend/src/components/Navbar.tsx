9dd6633fb07ab1df87ace86b8d5584867c2e4c35
Ricky Liang <liangricky7@gmail.com>
2025-08-22 05:02:08 -0500

    NAV MENU ANIM !!

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "motion/react"
+
 import purple_logo from "../assets/images/Logo_Purple.png";
-import { motion } from "motion/react"
-import Account_Icon from "../assets/images/account_circle.png"
 
 export const Navbar = () => {
     const [animate, setAnimate] = useState(false);
@@ -14,6 +14,7 @@ export const Navbar = () => {
         setNavMenuOpen(false);
     }, [location]);
 
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setAnimate(true);
@@ -21,78 +22,82 @@ export const Navbar = () => {
         
         return () => clearTimeout(timer);
     })
-    return (
-        <>
-            <nav
-                className="fixed h-[10vh] w-full flex z-50 items-center justify-center gap-[30vw]"
-            >
-                {/*  transition-transform duration-1000 ease-in
-                                    ${animate ? 'translate-y-0' : '-translate-y-[10vh]'} */}
-                <div className={`w-[11rem]`}>
-                    <div className={`h-[5.4vh] bg-[rgb(81,0,135)] flex items-center justify-center rounded-4xl text-3xl`}>
-                        <h2 className="text-[#F5F0F6]">Contact</h2>
-                    </div>
-                </div>
 
-                <div className={`w-[20rem] ${animate ? 'opacity-100' : 'opacity-0'}`}>
-                    <Link to="/">
-                        <img
-                            src={purple_logo}
-                            alt="Home Button"
-                            className="h-full object-cover"
-                        />
-                    </Link>
-                </div>
-                
-                <div className="w-[11rem]">
-                    <div
-                        className="w-full h-[5.4vh] bg-[rgb(81,0,135)] cursor-pointer flex items-center justify-center rounded-4xl text-3xl float-right"
-                        onClick={() => setNavMenuOpen(!navMenuOpen)}
-                    >
-                        <h2 className="text-[#F5F0F6] select-none">Menu</h2>
+    return (
+        <div className="fixed z-50">
+            <span className="w-screen flex justify-center">
+                <nav
+                    className="w-[95vw] h-[10vh] flex items-center justify-between"
+                >
+                    {/*  transition-transform duration-1000 ease-in
+                                        ${animate ? 'translate-y-0' : '-translate-y-[10vh]'} */}
+                    <div className={`w-[11rem]`}>
+                        <div className={`h-[5.4vh] bg-[rgb(81,0,135)] flex items-center justify-center rounded-4xl text-3xl`}>
+                            <h2 className="text-[#F5F0F6]">Contact</h2>
+                        </div>
                     </div>
-                </div>
-            </nav>
-            <div className={`${navMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-                <NavMenu/>
+
+                    <div className={`w-[20rem] ${animate ? 'opacity-100' : 'opacity-0'}`}>
+                        <Link to="/">
+                            <img
+                                src={purple_logo}
+                                alt="Home Button"
+                                className="h-full object-cover"
+                            />
+                        </Link>
+                    </div>
+                    
+                    <div className="w-[11rem]">
+                        <div
+                            className="w-full h-[5.4vh] bg-[rgb(81,0,135)] cursor-pointer flex items-center justify-center rounded-4xl text-3xl float-right"
+                            onClick={() => setNavMenuOpen(!navMenuOpen)}
+                        >
+                            <h2 className="text-[#F5F0F6] select-none">Menu</h2>
+                        </div>
+                    </div>
+                </nav>
+            </span>
+            <div className={`h-0 absolute right-12 overflow-hidden transition-all duration-1000 ease-in-out
+                ${navMenuOpen ? 'h-screen' : 'h-0 pointer-events-none'}`}>
+                <div className="w-fit h-full flex flex-col">
+                    <div className="w-full h-fit bg-[#510087] rounded-4xl !p-10 !pl-15 !pr-15">
+                        <div className="flex flex-col justify-between items-center">
+                            <NavPage 
+                                pageTitle="Home"
+                                pageURL="/"
+                            />
+                            <NavPage 
+                                pageTitle="About"
+                                pageURL="/about"
+                            />
+                            <NavPage 
+                                pageTitle="Cars"
+                                pageURL="/cars"
+                            />
+                            <NavPage 
+                                pageTitle="Team"
+                                pageURL="/team"
+                            />
+                            <NavPage 
+                                pageTitle="Join"
+                                pageURL="/join"
+                            />
+                            <NavPage 
+                                pageTitle="Sponsors"
+                                pageURL="/Sponsors"
+                            />
+                        </div>
+                    </div>
+
+                    {/* <div className="w-full h-fit bg-[#FFD500] rounded-4xl !p-10 !mt-[3vh] flex justify-center text-[6vh]">
+                        <h1 className="text-[#FFFFFFF]">Member Portal →</h1>
+                    </div> */}
+                </div>            
             </div>
 
-        </>
-    )
-}
-
-export const NavMenu = () => {
-    return (
-        <div className="w-[25vw] fixed right-8 top-30 z-40 bg-[#510087] rounded-4xl">
-            <div className="w-full h-[50vh] flex flex-col justify-between items-center">
-                <NavPage 
-                    pageTitle="Home"
-                    pageURL="/"
-                />
-                <NavPage 
-                    pageTitle="About"
-                    pageURL="/about"
-                />
-                <NavPage 
-                    pageTitle="Cars"
-                    pageURL="/cars"
-                />
-                <NavPage 
-                    pageTitle="Team"
-                    pageURL="/team"
-                />
-                <NavPage 
-                    pageTitle="Join"
-                    pageURL="/join"
-                />
-                <NavPage 
-                    pageTitle="Sponsors"
-                    pageURL="/Sponsors"
-                />
-            </div>
         </div>
     )
-};
+}
 
 const NavPage = (props) => {
     const {
@@ -113,11 +118,10 @@ const NavPage = (props) => {
     return (
         <Link 
             to={pageURL}
-                className="w-[90%] flex justify-between select-none cursor-pointer text-[3vw]"
+            className="w-full flex justify-between select-none cursor-pointer text-[6vh]"
         >
             <h2 className="text-[#F5F0F6]">{pageTitle}</h2>
             <h2 className={`${currentPage ? "opacity-100" : "opacity-0" } text-[#F5F0F6]`}>•</h2>
         </Link>
-
     )
 }
