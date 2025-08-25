108f200429ec300094822337fcf3742b74676a5a
Ricky Liang <liangricky7@gmail.com>
2025-08-25 10:50:47 -0500

    some team page init (i gave up bc no design)

--- frontend/src/pages/public/Team.tsx
+++ frontend/src/pages/public/Team.tsx
@@ -1,12 +1,17 @@
 const Team = () => {
   return (
-    <div className="w-screen">
-      <div className="!pt-[8vh] text-[#510087] text-center text-[25rem] leading-[25rem]">
+    <div className="w-screen text-[#510087]">
+      <div className="h-screen !pt-[8vh] text-center text-[25rem] leading-[25rem]">
         <h1>MEET THE TEAM.</h1>
       </div>
 
-      <div>
-        
+      <div className="w-[93%] !mx-auto !my-[3vh]">
+        <span className="text-7xl">
+          <h2>Admin</h2>
+        </span>
+        <span className="w-screen"> {/* i tried to use the hr tag but its not cooperating with tailwind so im doing this for now */}
+          <div className="w-[93vw] h-1 bg-[#510087] mx-auto"></div>
+        </span>
       </div>
     </div>
   );
