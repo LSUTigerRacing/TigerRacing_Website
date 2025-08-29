183b0dd3521a7f1a58e11fdaddeef856a8d3d557
Ricky Liang <liangricky7@gmail.com>
2025-08-29 16:59:26 -0500

    System Pages init

--- frontend/src/pages/public/join-subpages/Business.tsx
+++ frontend/src/pages/public/join-subpages/Business.tsx
@@ -1,7 +1,16 @@
 const Business = () => {
   return (
     <div className="w-screen h-fit flex flex-col justify-center !pt-[10vh]">
-        business
+      <div className="h-[85vh] flex flex-col justify-between items-center text-center">
+        <span className="text-[#510087] text-[10rem] leading-[10rem]">
+          <h1>THE ENGINE BEHIND THE ENGINE.</h1>
+        </span>
+  
+        <span className="text-[2.2rem] leading-[2rem]">
+          <p>SCROLL TO EXPLORE BUSINESS</p>
+        </span>
+
+      </div>
     </div>
   );
 };
--- frontend/src/pages/public/join-subpages/Chassis.tsx
+++ frontend/src/pages/public/join-subpages/Chassis.tsx
@@ -1,7 +1,16 @@
 const Business = () => {
   return (
     <div className="w-screen h-fit flex flex-col justify-center !pt-[10vh]">
-        chassis
+      <div className="h-[85vh] flex flex-col justify-between items-center text-center">
+        <span className="text-[#510087] text-[14rem] leading-[14rem]">
+          <h1>FOUNDATION OF VICTORY.</h1>
+        </span>
+  
+        <span className="text-[2.2rem] leading-[2rem]">
+          <p>SCROLL TO EXPLORE CHASSIS</p>
+        </span>
+
+      </div>
     </div>
   );
 };
