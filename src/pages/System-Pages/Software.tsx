c20a9be5154b00cd21c5ce8c0e901de5bc38616c
Ricky Liang <liangricky7@gmail.com>
2026-01-03 13:31:25 -0600

    feat: add basic software page

--- src/pages/System-Pages/Software.tsx
+++ src/pages/System-Pages/Software.tsx
@@ -1,7 +1,50 @@
+import Landing from "../../assets/images/System-Pages/Chassis/Landing.png";
+import TempPicture from "../../assets/images/System-Pages/Powertrain/PCB.png";
+
+import { LandingComponent, SubsystemComponent, TableOfContents } from "./SystemComponents";
+
 const Software = () => {
   return (
-    <div className="w-full background">
+ <div className="w-full background">
+        <LandingComponent 
+			name="Software"
+			blurb="The LSU TigerRacing team sincerely thanks all of our generous sponsors. We could not build our electric vehicles, maintain our combustion cars, and thrive as one of LSU's premiere engineering organizations without the help of our sponsors.
+			
+			If you are interested in becoming a TigerRacing sponsor, please look at our sponsorship packet linked below and contact our team at 
+			
+			Your support makes a real difference to our team."
+			image={Landing} 
+		/>
 
+        <TableOfContents
+            sections={[
+                { index: "01", title: "App Development" },
+                { index: "02", title: "Embedded Controls" },
+                { index: "03", title: "Data Analysis" },
+            ]}
+        />
+
+        <SubsystemComponent
+            index="01"
+            title="App Development"
+            description="Formula SAE is about much more than just engineering. Being a part of the Business division provides an unparalleled collegiate experience in all aspects of running a racing team including financial management, procurement, sponsorship and more. Business division members gain valuable experience with recruitment, event planning, content creation, social media management, procurement of materials, establishing and maintaining relationships with sponsors, and managing the team’s finances. Members develop their speaking skills through many presentations to alumni, stakeholders, and judges. In addition, a major portion of the Formula SAE competition is a business presentation based on a chosen prompt to a prospective client. "
+            images={[TempPicture, TempPicture, TempPicture]} 
+            layout={2}
+        />
+        <SubsystemComponent
+            index="02"
+            title="Embedded Controls"
+            description="Formula SAE is about much more than just engineering. Being a part of the Business division provides an unparalleled collegiate experience in all aspects of running a racing team including financial management, procurement, sponsorship and more. Business division members gain valuable experience with recruitment, event planning, content creation, social media management, procurement of materials, establishing and maintaining relationships with sponsors, and managing the team’s finances. Members develop their speaking skills through many presentations to alumni, stakeholders, and judges. In addition, a major portion of the Formula SAE competition is a business presentation based on a chosen prompt to a prospective client. "
+            images={[TempPicture, TempPicture, TempPicture]} 
+            layout={2}
+        />
+        <SubsystemComponent
+            index="03"
+            title="Data Analysis"
+            description="Formula SAE is about much more than just engineering. Being a part of the Business division provides an unparalleled collegiate experience in all aspects of running a racing team including financial management, procurement, sponsorship and more. Business division members gain valuable experience with recruitment, event planning, content creation, social media management, procurement of materials, establishing and maintaining relationships with sponsors, and managing the team’s finances. Members develop their speaking skills through many presentations to alumni, stakeholders, and judges. In addition, a major portion of the Formula SAE competition is a business presentation based on a chosen prompt to a prospective client. "
+            images={[TempPicture, TempPicture, TempPicture]} 
+            layout={2}
+        />
     </div>
   );
 };
